91359ff2a0f18e3dcad3b041ba182fcfb7b68bab
Fredrik Welander <fredrik.welander@subsite.fi>
2023-05-17 22:21:53 +0300

    add CORS

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask, request, escape
+from flask_cors import CORS 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def index():

7afca63ec637b4b5ff9513d460658b13a0d9da53
Fredrik Welander <fredrik.welander@subsite.fi>
2023-05-17 21:26:40 +0300

    pastebin v1

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, escape
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    ret = {'msg': 'Rahti Flask demo works.', 'env_var': os.environ.get('ENV_VAR') }
+    ret = {'msg': 'Pastebin API endpoint /pastebin, needs key. POST as JSON: { "type": "str", "val": "your value" } (type can be omitted if str)' }
 
     return ret
 
 
-@app.route("/test-route", methods = [ 'GET', 'POST' ])
+pastebin = ['nothing here']
+@app.route("/pastebin", methods = [ 'GET', 'POST' ])
 def test_route():
 
-    if request.method == 'POST':
-    
+    if request.args.get('api_key') != os.environ.get('API_KEY'):
+        return { 'msg': 'ERROR: Bad API key' }, 401
+
+    if request.method == 'GET':
         try:
-            ret =  { 'msg': 'POST works', 'postdata': request.get_json() }
+            idx = request.args.get('idx') or 0
+            ret = escape(pastebin[int(idx)])
+        except Exception as e:
+            print(repr(e))
+            ret = { 'msg': 'ERROR: GET failed' }, 500
+
+    if request.method == 'POST':
+        
+        try:
+            pasted_value = request.get_json()
+            pastebin.insert(0, pasted_value['val'])
+            ret = {'msg': 'OK'}
 
         except Exception as e:
-            print(e)
-            ret =  { 'msg': 'ERROR: POST failed.' }, 500
-
-    else:
-        ret = {'msg': 'Test route GET'}
+            print(repr(e))
+            ret = { 'msg': 'ERROR: POST failed (POST as JSON: { "type": "str", "val": "your value" } )' }, 500
 
     return ret
 
